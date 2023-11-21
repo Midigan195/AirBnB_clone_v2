75abe637f982a3749096ab2c66de7743e4ea9440
Garnet Matola <matolagarnet.com>
2023-11-21 05:54:33 -0800

    Updated places

--- models/place.py
+++ models/place.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy import Column, String, Integer, Float, ForeignKey
 from models.base_model import BaseModel, Base
 from sqlalchemy.ext.declarative import declarative_base
+from models.city import City
+from models.user import User
 
 
 class Place(BaseModel, Base):
@@ -13,11 +15,11 @@ class Place(BaseModel, Base):
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
 
     name = Column(String(128), nullable=False)
-    description = Column(String(1024), nullable=False)
+    description = Column(String(1024), nullable=True)
     number_rooms = Column(Integer, nullable=False, default=0)
     number_bathrooms = Column(Integer, nullable=False, default=0)
     max_guest = Column(Integer, nullable=False, default=0)
     price_by_night = Column(Integer, nullable=False, default=0)
-    latitude = Column(Float, nullable=False)
-    longitude = Column(Float, nullable=False)
-    amenity_ids = []
+    latitude = Column(Float, nullable=True)
+    longitude = Column(Float, nullable=True)
+    #amenity_ids = []
